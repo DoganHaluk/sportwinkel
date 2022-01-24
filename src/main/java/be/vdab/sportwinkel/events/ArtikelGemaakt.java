9053b1469092a83d425f4ecd3208046745cdaaca
DoganHaluk <halukdogan78@gmail.com>
2022-01-24 13:41:40 +0100

    verkoopprijs added to the message

--- src/main/java/be/vdab/sportwinkel/events/ArtikelGemaakt.java
+++ src/main/java/be/vdab/sportwinkel/events/ArtikelGemaakt.java
@@ -2,13 +2,17 @@ package be.vdab.sportwinkel.events;
 
 import be.vdab.sportwinkel.domain.Artikel;
 
+import java.math.BigDecimal;
+
 public class ArtikelGemaakt {
     private final long id;
     private final String naam;
+    private final BigDecimal verkoopprijs;
 
     public ArtikelGemaakt(Artikel artikel) {
         this.id = artikel.getId();
         this.naam = artikel.getNaam();
+        this.verkoopprijs = artikel.getVerkoopprijs();
     }
 
     public long getId() {
@@ -17,5 +21,9 @@ public class ArtikelGemaakt {
 
     public String getNaam() {
         return naam;
+    }
+
+    public BigDecimal getVerkoopprijs() {
+        return verkoopprijs;
     }
 }
